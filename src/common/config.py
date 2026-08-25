6ab9a1724d67912310aed37aeb2c5baef5f09ea4
tasrif_30217 <mir.tasrif@bjitacademy.com>
2026-08-25 17:20:55 +0600

    Add configuration and logging foundation

---
+++ src/common/config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+APP_ENV = os.getenv("APP_ENV", "development")
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+MAX_PRODUCTS = int(os.getenv("MAX_PRODUCTS", "100"))
+REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", "30"))
+#The first argument is the environment variable name.
+#The second argument is a default value.
