d6608f7a21fe5e76e698db2f4f6b3c6e9c849dba
Tasrif <mir.tasrif.cse@gmail.com>
2026-08-26 15:03:17 +0600

    Add data collection models and exceptions

---
+++ src/common/exceptions.py
@@ -0,0 +1,18 @@
+class CollectionError(Exception):
+    """Base exception for data collection errors."""
+
+
+class RequestError(CollectionError):
+    """Raised when a collection request fails."""
+
+
+class BlockedPageError(CollectionError):
+    """Raised when collection is blocked by Amazon."""
+
+
+class ParsingError(CollectionError):
+    """Raised when expected page data cannot be parsed."""
+
+
+class ValidationError(CollectionError):
+    """Raised when collected data fails validation."""
